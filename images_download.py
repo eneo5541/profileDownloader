5855c368e1b3f31123c2986e5ae3920f9bea64c6
Eric <neo_eric@hotmail.com>
2015-06-03 20:17:14 +1000

    Now downloads all content (text, flash, etc) not just images

--- images_download.py
+++ images_download.py
@@ -2,53 +2,57 @@
 
 import html.parser
 import urllib.request
-import os
 
-class FavouritePageParser(html.parser.HTMLParser):
+class PageParser(html.parser.HTMLParser):
 	def handle_starttag(self, tag, attr):
 		if tag.lower() == "a":
 			for item in attr:
 				if item[0].lower() == "href" and item[1].startswith("/view/"):
 					links.append("http://www.test.net/full/"+item[1].split('/')[2]+"/")
 
-class ImagePageParser(html.parser.HTMLParser):
+class ImageParser(html.parser.HTMLParser):
+	imgUrl = ""
+	
 	def handle_starttag(self, tag, attr):
-		if tag.lower() == "img":
-			if attr[0][0] == "id" and attr[0][1] == "submissionImg" and attr[3][0] == "src":
-				imgUrl = attr[3][1]
-				fileName = imgUrl.split('/')[-1]
-				if imgUrl.startswith("//"):
-					imgUrl = "http:" + imgUrl
-				
-				fullfilename = os.path.join(destination, fileName)
-				urllib.request.urlretrieve(imgUrl, fullfilename)
+		if tag.lower() == "a":
+			self.imgUrl = attr[0][1]
+	
+	def handle_data(self, data):
+		if (data.strip().lower() == "download"):
+			fileName = self.imgUrl.split('/')[-1]
+			if self.imgUrl.startswith("//"):
+				self.imgUrl = "http:" + self.imgUrl
+			urllib.request.urlretrieve(self.imgUrl, fileName)
 
 def init():
-	global targetUrl
-	targetUrl = "http://www.test.net/faves/" + input('User favourites to download? ')
-	global destination
-	destination = input('Destination for images? ')
+	global targetPage
+	targetPage = input('Page to download? (favorites/gallery)')
+	if targetPage.lower() == "favourites":
+		targetPage = "favorites"
 	
-	confirm = input('Download contents of ' + targetUrl + " to " + destination + ". Y/N/Abort?")
+	global targetUrl
+	targetUrl = "http://www.test.net/" + targetPage + "/" + input('User page to download? ')
+	
+	confirm = input('Download contents of ' + targetUrl + "? (Y/N/abort)")
 	if confirm.lower() == "y" or confirm.lower() == "yes":
-		getFavouriteLinks(targetUrl)
+		getPageLinks(targetUrl)
 		parseLinks()
 	elif confirm.lower() == "n" or confirm.lower() == "no":
 		init()
 	else:
 		print("Aborting")
 
-def getFavouriteLinks( url ):
+def getPageLinks(url):
 	i = 1
 	pageLinks = getLinks(url + "/" + str(i) + "/")
 	prevLinks = 0
 	print("Reading in ",len(links)," assets...")
 	
-	# while len(links) > prevLinks:
-		# prevLinks = len(links)
-		# i += 1
-		# pageLinks = getLinks(url + "/" + str(i) + "/")
-		# print("Reading in ",len(links)," assets...")
+	while len(links) > prevLinks:
+		prevLinks = len(links)
+		i += 1
+		pageLinks = getLinks(url + "/" + str(i) + "/")
+		print("Reading in ",len(links)," assets...")
 	return
 
 def getLinks( url ):
@@ -56,7 +60,7 @@ def getLinks( url ):
 	response = urllib.request.urlopen(request)
 	html = response.read()
 	
-	parser = FavouritePageParser()
+	parser = PageParser()
 	parser.feed(str(html))
 	return
 	
@@ -69,12 +73,11 @@ def parseLinks():
 		response = urllib.request.urlopen(request)
 		html = response.read()
 		
-		parser = ImagePageParser()
+		parser = ImageParser()
 		parser.feed(str(html))
 	
 	print("Downloading completed")
 	return
-
 
 global links
 links = []
